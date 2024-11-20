1d9ef13c4c1707b5416c77ed43969b8c32845e0a
Vaibhav vats <vabhavvats30@gmail.com>
2024-11-20 15:31:01 +0530

    11. Post request is working fine. A new student added successfully. Also error handling is done here for the same email present in DB.

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,4 +1,42 @@
-const express = require('express');
+const express = require("express");
+const Student = require("../models/studentSchema");
 
 const router = express.Router();
 
+// Post route to add a new student
+router.post("/students", async (req, res) => {
+  try {
+    const { name, email, classId, profileImageUrl } = req.body;
+
+    // Validate the required feilds
+    if (!name || !email || !classId) {
+      return res
+        .status(400)
+        .json({ message: "Name, email, and classId are required." });
+    }
+
+    // check if the email already exists
+    const existingStudent = await Student.findOne({ email });
+    if (existingStudent) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    // create a new student document
+    const newStudent = new Student({ name, email, classId, profileImageUrl });
+
+    // save newStudent in database
+    await newStudent.save();
+
+    // Respond with the newly created student
+    res.status(201).json({
+      message: "Student added successfully",
+      student: newStudent,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(501).json({ message: "Server error", error: err.message });
+  }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const express = require("express");
 require("dotenv").config();
 const connectToDatabase = require("./config/mongoDB");
 const messageSent = require('./messageUI');
+const studentRoutes = require('./routes/studentRoute');
 
 // express setup
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 // setup the first route
 app.get("/", (req, res) => {
   res.send(messageSent);
 });
+
+// Use the imported routes for students
+app.use("/api", studentRoutes);
 
 // start server
 const startServer = async () => {

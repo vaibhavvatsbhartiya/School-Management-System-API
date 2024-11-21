d5719130640d29bf5c9b47ef61daedec2c2ce2f7
Vaibhav vats <vabhavvats30@gmail.com>
2024-11-21 12:41:00 +0530

    28. Althougn code is written but the put and the delete route is not working properly.

--- routes/classRoute.js
+++ routes/classRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const Teacher = require("../models/teacherSchema");
 const Class = require("../models/classSchema");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const router = express.Router();
 
@@ -65,6 +65,57 @@ router.get("/classes/:id", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error fetching class", error });
+  }
+});
+
+// 4. **PUT** /classes/:id - Update class details : ********* I don't to its working or not as its not verify yet.*********
+// router.put("/classes/:id", async (req, res) => {
+//   try {
+//     const classId = req.params.id;
+//     const { name, teacherId, studentCount, grade } = req.body;
+
+//     if (!mongoose.Types.ObjectId.isValid(classId)) {
+//       return res.status(400).json({ message: "Invalid class ID" });
+//     }
+
+//     const updatedClass = await Class.findByIdAndUpdate(
+//       classId,
+//       { name, teacherId, studentCount, grade },
+//       { new: true, runValidators: true }
+//     );
+
+//     if (!updatedClass) {
+//       return res.status(404).json({ message: "Class not found" });
+//     }
+
+//     res.status(200).json(updatedClass);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Error updating class", error });
+//   }
+// });
+
+// 5. **DELETE** /classes/:id - Delete a class by ID
+router.delete("/classes/permanent/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // if (!mongoose.Types.ObjectId.isValid(id)) {
+    //   return res.status(400).json({ message: "Invalid class ID" });
+    // }
+    const classs = await Class.findByIdAndDelete(id);
+
+    if (!classs) {
+      return res.status(404).json({ message: "class not found" });
+    }
+
+    res.status(200).json({
+      message: "Classs's data permanently deleted successfully",
+      classs,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 });
 

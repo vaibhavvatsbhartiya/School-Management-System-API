abf7c17123d786906d1a425aa52903d82073ad10
Vaibhav vats <vabhavvats30@gmail.com>
2024-11-22 16:40:06 +0530

    41. teacherRoutes Post,PUT & DELETE these are protected now and I didn't add protection to GET route as i think user can see atleast some data when API is LIVE.

--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -8,10 +8,12 @@ const {
   permanentDeleteTeacher,
 } = require("../controllers/teacherController");
 
+const verifyToken = require('../jwt_auth/jwt');
+
 const router = express.Router();
 
 //  POST route to add a new teacher
-router.post("/teachers/new", addTeacher);
+router.post("/teachers/new", verifyToken("Admin"), addTeacher);
 
 //  GET route to get all teachers
 router.get("/teachers", getAllTeachers);
@@ -20,12 +22,12 @@ router.get("/teachers", getAllTeachers);
 router.get("/teachers/:id", getTeacherById);
 
 // PUT route to update teacher details by ID
-router.put("/teachers/:id", updateTeacher);
+router.put("/teachers/:id", verifyToken("Admin"), updateTeacher);
 
 // DELETE route to soft delete a student by ID
-router.delete("/teachers/:id", softDeleteTeacher);
+router.delete("/teachers/:id", verifyToken("Admin"), softDeleteTeacher);
 
 // DELETE route for permanent deletion
-router.delete("/teachers/permanent/:id", permanentDeleteTeacher);
+router.delete("/teachers/permanent/:id", verifyToken("Admin"), permanentDeleteTeacher);
 
 module.exports = router;

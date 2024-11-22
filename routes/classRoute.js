e21d7cd540f3e8d6283b9da49e2afdd6e6da9ec7
Vaibhav vats <vabhavvats30@gmail.com>
2024-11-22 16:46:35 +0530

    42. student and class routes's  Post,PUT & DELETE these are protected now and I didn't add protection to GET route as i think user can see atleast some data when API is LIVE.

--- routes/classRoute.js
+++ routes/classRoute.js
@@ -8,10 +8,13 @@ const {
 } = require("../controllers/classController");
 
 
+const verifyToken = require('../jwt_auth/jwt');
+
+
 const router = express.Router();
 
 // 1. **POST** /classes - Create a new class
-router.post("/classes/new", createClass);
+router.post("/classes/new", verifyToken("Admin"), createClass);
 
 // 2. **GET** /classes - Get all classes
 router.get("/classes", getAllClasses);
@@ -20,9 +23,9 @@ router.get("/classes", getAllClasses);
 router.get("/classes/:id", getClassById);
 
 // 4. **PUT** /classes/:id - Update class details : ********* I don't to its working or not as its not verify yet.*********
-router.put("/classes/:id", updateClass);
+router.put("/classes/:id", verifyToken("Admin"), updateClass);
 
 // 5. **DELETE** /classes/:id - Delete a class by ID
-router.delete("/classes/permanent/:id", permanentDeleteClass);
+router.delete("/classes/permanent/:id", verifyToken("Admin"), permanentDeleteClass);
 
 module.exports = router;
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -8,10 +8,12 @@ const {
   permanentDeleteStudentById,
 } = require('../controllers/studentController');
 
+const verifyToken = require('../jwt_auth/jwt');
+
 const router = express.Router();
 
 // Post route to add a new student
-router.post("/students/new", addStudent);
+router.post("/students/new", verifyToken("Admin"), addStudent);
 
 // GET route to get all students
 router.get("/students", getAllStudents);
@@ -20,13 +22,13 @@ router.get("/students", getAllStudents);
 router.get("/students/:id", getStudentById);
 
 // PUT route to update student details by ID
-router.put("/students/:id", updateStudentById);
+router.put("/students/:id", verifyToken("Admin"), updateStudentById);
 
 // DELETE route to soft delete a student by ID
-router.delete("/students/:id", softDeleteStudentById);
+router.delete("/students/:id", verifyToken("Admin"), softDeleteStudentById);
 
 // DELETE route for permanent deletion
-router.delete("/students/permanent/:id", permanentDeleteStudentById);
+router.delete("/students/permanent/:id", verifyToken("Admin"), permanentDeleteStudentById);
 
 
 module.exports = router;
